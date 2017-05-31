0d99b8bfa6f4816a40a6901fd7742fa7855c2096
iceiceiceice <335998869@qq.com>
2017-05-31 16:03:53 +0800

    销户部分

--- public/AdminLTE-2.3.11/dist/js/discard.js
+++ public/AdminLTE-2.3.11/dist/js/discard.js
@@ -3,7 +3,7 @@
  */
 $().ready(function () {
 
-    $("#password_form").validate({
+    $("#discard_form").validate({
         submitHandler : function(form) {  //验证通过后的执行方法
             //当前的form通过ajax方式提交（用到jQuery.Form文件）
             //alert("提交表单");
@@ -12,43 +12,42 @@ $().ready(function () {
                 dataType:"json",
                 success:function( jsondata ){
                     if( jsondata.code == 200 ){
-                        alert("修改成功123");
+                        alert("已经提交销户请求，等待管理员审核");
                     }else{
-                        alert("修改失败");
+                        alert("销户申请失败");
                     }
                 }
             });
 
         },
         rules: {
-            oldpassword: {
+            name: {
+                required: true
+            },
+            idnumber: {
+                required : true,
+                minlength: 18,
+                maxlength: 18
+            },
+            password: {
                 required: true,
                 minlength: 6,
                 maxlength: 20
-            },
-            newpassword: {
-                required: true,
-                minlength: 6,
-                maxlength: 20
-            },
-            repassword: {
-                equalTo: "#new_password"
             }
         },
         messages: {
-            oldpassword: {
-                required: "*请旧密码",
-                minlength: jQuery.validator.format("*用户名不能小于{0}个字符"),
-                maxlength: jQuery.validator.format("*用户名不能大于{0}个字符"),
-                notnumber: "*用户名不能是纯数字"
+            name: {
+                required: "*请输入姓名"
             },
-            newpassword: {
-                required: "*请输入新密码",
+            idnumber:{
+                required: "*请输入身份证号",
+                minlength: jQuery.validator.format("*请输入正确的身份证号"),
+                maxlength: jQuery.validator.format("*请输入正确的身份证号")
+            },
+            password: {
+                required: "*请输入密码",
                 minlength: jQuery.validator.format("*密码不能小于{0}个字符"),
                 maxlength: jQuery.validator.format("*密码不能大于{0}个字符")
-            },
-            repassword: {
-                equalTo: "*两次密码不同"
             }
         }
 
